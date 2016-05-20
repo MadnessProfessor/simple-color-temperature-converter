2c89534fdbfa978daa3b83c6c31a205081c3549a
Misha Schmidt <misha.schmidtz@gmail.com>
2016-05-20 23:17:37 +0200

    switched to jQuery

--- pageFunctionality.js
+++ pageFunctionality.js
@@ -1,8 +1,13 @@
 "use strict";
 function convert()
 {
-	let RGB = colorTemperatureToRGB(parseFloat(document.getElementById('textField').value)).r.toFixed().toString() + ', ' + colorTemperatureToRGB(parseFloat(document.getElementById('textField').value)).g.toFixed().toString() + ', ' + colorTemperatureToRGB(parseFloat(document.getElementById('textField').value)).b.toFixed().toString();
+	let RGB = colorTemperatureToRGB(parseFloat($('#textField').val())).r.toFixed().toString() + ', ' + colorTemperatureToRGB(parseFloat($('#textField').val())).g.toFixed().toString() + ', ' + colorTemperatureToRGB(parseFloat($('#textField').val())).b.toFixed().toString();
 	console.log(RGB);
-	document.getElementById('output').innerHTML = 'RGB (dec) = ' + RGB + '<br>RGB (hex) = ' + parseInt(colorTemperatureToRGB(parseFloat(document.getElementById('textField').value)).r.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(colorTemperatureToRGB(parseFloat(document.getElementById('textField').value)).g.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(colorTemperatureToRGB(parseFloat(document.getElementById('textField').value)).b.toFixed()).toString(16).toUpperCase();
-	document.getElementById('right').style.backgroundColor = 'rgb(' + RGB + ')';
+	$('#output').html('RGB (dec) = ' + RGB + '<br>RGB (hex) = ' + parseInt(colorTemperatureToRGB(parseFloat($('#textField').val())).r.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(colorTemperatureToRGB(parseFloat($('#textField').val())).g.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(colorTemperatureToRGB(parseFloat($('#textField').val())).b.toFixed()).toString(16).toUpperCase());
+	$('#right').css('background-color', 'rgb(' + RGB + ')');
 }
+$(function()
+{
+	$('#submitButton').click(convert);
+	$(this).find('input').keypress(function(e){if(e.which == 10 || e.which == 13){convert();}});
+});
