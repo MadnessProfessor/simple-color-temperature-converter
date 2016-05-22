510eac4a7deb7f56922943624bcf1c3d96bb3f0f
Misha Schmidt <misha.schmidtz@gmail.com>
2016-05-22 15:55:52 +0200

    added one more output

--- colorTempToRGB.js
+++ colorTempToRGB.js
@@ -1,61 +1,62 @@
-// From http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+// Based on http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
 
-    // Start with a temperature, in Kelvin, somewhere between 1000 and 40000.  (Other values may work,
-    //  but I can't make any promises about the quality of the algorithm's estimates above 40000 K.)
-
-    
-function colorTemperatureToRGB(kelvin){
-
-    var temp = kelvin / 100;
-
-    var red, green, blue;
-
-    if( temp <= 66 ){ 
-
-        red = 255; 
-        
-        green = temp;
-        green = 99.4708025861 * Math.log(green) - 161.1195681661;
-
-        
-        if( temp <= 19){
-
-            blue = 0;
-
-        } else {
-
-            blue = temp-10;
-            blue = 138.5177312231 * Math.log(blue) - 305.0447927307;
-
-        }
-
-    } else {
-
-        red = temp - 60;
-        red = 329.698727446 * Math.pow(red, -0.1332047592);
-        
-        green = temp - 60;
-        green = 288.1221695283 * Math.pow(green, -0.0755148492 );
-
-        blue = 255;
-
-    }
-
-
-    return {
-        r : clamp(red,   0, 255),
-        g : clamp(green, 0, 255),
-        b : clamp(blue,  0, 255)
-    }
-
-}
-
+// Start with a temperature, in Kelvin, somewhere between 1000 and 40000.  (Other values may work,
+//  but I can't make any promises about the quality of the algorithm's estimates above 40000 K.)
 
 function clamp( x, min, max ) {
+	if(x<min){ return min; }
+	if(x>max){ return max; }
+	
+	return x;
+}
 
-    if(x<min){ return min; }
-    if(x>max){ return max; }
+function colorTemperatureToRGB(temperature, scale)
+{
+	// define vars
+	var temp = temperature / 100;
+	var red, green, blue;
+	
+	// set RGB values
+	if( temp <= 66 )
+	{
+		red = 1; 
+		
+		green = 0.390081579 * Math.log(temp) - 0.631841444;
+		
+		if( temp <= 19)
+		{
+			blue = 0;
+		} else
+		{
+			blue = 0.543206789 * Math.log(temp-10) - 1.196254089;
+		}
 
-    return x;
+	} else 
+	{
+		red = 1.292936186 * Math.pow(temp - 60, -0.1332047592);
+		
+		green = 1.129890861 * Math.pow(temp - 60, -0.0755148492 );
+		
+		blue = 1;
+	}
+	
+	// scale if necessary
+	if(scale)
+	{
+		red = clamp(red*255, 0, 255);
+		green = clamp(green*255, 0, 255);
+		blue = clamp(blue*255, 0, 255);
+	} else
+	{
+		red = clamp(red, 0, 1);
+		green = clamp(green, 0, 1);
+		blue = clamp(blue, 0, 1);
+	}
+	
+    return {
+        r : red,
+        g : green,
+        b : blue
+    }
 
 }
--- pageFunctionality.js
+++ pageFunctionality.js
@@ -1,13 +1,25 @@
 "use strict";
+
 function convert()
 {
-	let RGB = colorTemperatureToRGB(parseFloat($('#textField').val())).r.toFixed().toString() + ', ' + colorTemperatureToRGB(parseFloat($('#textField').val())).g.toFixed().toString() + ', ' + colorTemperatureToRGB(parseFloat($('#textField').val())).b.toFixed().toString();
-	console.log(RGB);
-	$('#output').html('RGB (dec) = ' + RGB + '<br>RGB (hex) = ' + parseInt(colorTemperatureToRGB(parseFloat($('#textField').val())).r.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(colorTemperatureToRGB(parseFloat($('#textField').val())).g.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(colorTemperatureToRGB(parseFloat($('#textField').val())).b.toFixed()).toString(16).toUpperCase());
-	$('#right').css('background-color', 'rgb(' + RGB + ')');
+	let sC = colorTemperatureToRGB(parseFloat($('#textField').val()),true);
+	let strSCDec = sC.r.toFixed() + ', ' + sC.g.toFixed() + ', ' + sC.b.toFixed();
+	
+	let strSCHex = parseInt(sC.r.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(sC.g.toFixed()).toString(16).toUpperCase() + ', ' + parseInt(sC.b.toFixed()).toString(16).toUpperCase();
+	
+	let NsC = colorTemperatureToRGB(parseFloat($('#textField').val()), false);
+	let strNsCDec = NsC.r.toFixed(3) + ', ' + NsC.g.toFixed(3) + ', ' + NsC.b.toFixed(3);
+	
+	console.log('sC:  ' + strSCDec);
+	console.log('NsC: ' + strNsCDec);
+	
+	$('#output').html('RGB decimal =<br>' + strNsCDec + '<br><br>RGB decimal, scaled =<br>' + strSCDec + '<br><br>RGB hexadecimal, scaled =<br>' + strSCHex);
+	$('#right').css('background-color', 'rgb(' + strSCDec + ')');
 }
 $(function()
 {
 	$('#submitButton').click(convert);
 	$(this).find('input').keypress(function(e){if(e.which == 10 || e.which == 13){convert();}});
+	
+	console.log('Welcome to my online color temperature converter.\n\nFor more advanced usage of the code in this page use colorTemperatureToRGB(num temperature, bool scale).\nThe code in this page is based off of http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/. I recommend going to that page, it\'s a good read.\nTo view my conversion algorithm look at colorTempToRGB.js.');
 });
